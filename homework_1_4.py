16449c085656aa82df7a46841ee05f65664a2ae4
Anna Chernyshenko <a.g.chernyshenko@gmail.com>
2020-01-22 20:21:24 +0300

    Добавлены варианты решения задания №4

--- homework_1_4.py
+++ homework_1_4.py
@@ -2,6 +2,7 @@
 # Найдите самую большую цифру в числе.
 # Для решения используйте цикл while и арифметические операции.
 
+#Решение №1
 user_number = int(input("Введите число: "))
 m = user_number % 10
 user_number = user_number//10
@@ -10,3 +11,15 @@ while user_number > 0:
         m = user_number % 10
     user_number = user_number//10
 print(m)
+
+#Решение №2
+user_number = input("Введите число: ")
+print(max(user_number))
+
+#Решение №3
+user_number = input("Введите число: ")
+x = 0
+for i in user_number:
+    while int(i) > x:
+        x = int(i)
+print(x)
