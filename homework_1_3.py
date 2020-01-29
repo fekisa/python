cf92973dd3d7a91c800f4eee476a7f8d3a60b186
Anna Chernyshenko <a.g.chernyshenko@gmail.com>
2020-01-29 19:48:42 +0300

    удалены дубликаты

--- homework_1_3.py
+++
@@ -1,7 +0,0 @@
-#3. Узнайте у пользователя число n.
-# Найдите сумму чисел n + nn + nnn.
-# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-
-user_number = input("Введите число: ")
-sum = int(user_number) + int(user_number*2) + int(user_number*3)
-print(sum)
